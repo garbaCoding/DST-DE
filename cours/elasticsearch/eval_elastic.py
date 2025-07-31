29465a966e3a97a6ca1add91fab9f117df7ad0bd
garbaCoding <garbacoding@gmail.com>
2025-07-31 16:41:37 +0200

    Push final elasticsearch

--- cours/elasticsearch/eval_elastic.py
+++ cours/elasticsearch/eval_elastic.py
@@ -11,7 +11,7 @@ client = Elasticsearch(hosts = "http://localhost:9200")
 
 # Préciser le numéro de votre question ici.
 # Si vous effectuez plusieurs requêtes pour la même question, écrivez "1-1", "1-2" ext...
-question_number = "5-1"
+question_number = "5-4"
 
 query_max = {
     "size": 0,
@@ -22,31 +22,39 @@ query_max = {
     }
 }
 
+query_min = {
+    "size": 0,
+    "aggs": {
+        "min_rating": {
+            "min": { "field": "Rating" }
+        }
+    }
+}
+
 res_max = client.search(index="eval", body=query_max)
 note_max = res_max["aggregations"]["max_rating"]["value"]
-print("Note maximale présente dans le dataset :", note_max)
 
+res_min = client.search(index="eval", body=query_min)
+note_min = res_min["aggregations"]["min_rating"]["value"]
+print("Note maximale présente dans le dataset :", note_max)
+print("Note minimale présente dans le dataset :", note_min)
 
 # Copier coller votre requête Kibana ici (SANS l'instruction GET)
 query = {
-    "size": 0,
-    "query": {
-        "term": {
-            "Rating": note_max
-        }
-    },
-    "aggs": {
-        "top_class_names": {
-            "terms": {
-                "field": "Class Name.keyword"
-            }
-        },
-        "top_terms_review_text": {
-            "terms": {
-                "field": "Review Text"
-            }
-        }
+  "size": 0,
+  "query": {
+    "range": {
+      "Rating": { "lte": 2 }
     }
+  },
+  "aggs": {
+    "par_produit": {
+      "terms": { "field": "Class Name.keyword" },
+      "aggs": {
+        "nombre_avis": { "value_count": { "field": "Rating" } }
+      }
+    }
+  }
 }
 
 response = client.search(index="eval", body=query)
